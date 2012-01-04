3e4761e16749206c949f8c6cc37a6bf70d7ce401
puspa <spypuzpa@gmail.com>
2012-01-04 12:28:57 -0800

    edit view untuk setoran

--- puspa/application/views/v_setor.php
+++ puspa/application/views/v_setor.php
@@ -83,7 +83,7 @@ jQuery(function($) {
 		  <article id="dashboard">
 				<h1 align="center">Cetak Rekap Setoran </h1>
 			
-				<form name="form1" method="post" action="<?php echo site_url("tlain2/antarjemput/");?>">
+				<form name="form1" method="post" action="<?php echo site_url("tsetor/pilih_report_tunggakan/");?>">
 				  <label>				  </label>
 				  <table width="100%" border="0" align="center">
                     <tr>
@@ -152,7 +152,7 @@ jQuery(function($) {
                       <td><strong>Unit</strong></td>
                       <td>&nbsp;</td>
                       <td colspan="4"><select name="tx_unit" id="tx_unit">
-                        <option value=''>TK/SD/SMP</option>
+                        <option value='0'>TK/SD/SMP</option>
                         <?php 
 						foreach($data_unit->result() as $value){
 							echo "<option value='".$value->id."'>".$value->nama."</option>";
@@ -174,7 +174,9 @@ jQuery(function($) {
                       <td colspan="2"><div align="center"></div></td>
                       <td width="211"><a href="javascript:NewCssCal('tx_mulai','yyyymmdd')">
                         <input type="Text" name="tx_mulai" id="tx_mulai" maxlength="25" size="20">
-                        <img src="<?php echo base_url();?>datepicker/images/cal.gif" width="16" height="16" alt="Pick a date"></a><a href="javascript:NewCssCal('ttl','ddmmmyyyy')"></a></td>
+                        <img src="<?php echo base_url();?>datepicker/images/cal.gif" width="16" height="16" alt="Pick a date"></a><a href="javascript:NewCssCal('tx_akhir','yyyymmdd')">
+                        <input name="tx_mulai2" type="hidden" id="tx_mulai2">
+                        </a><a href="javascript:NewCssCal('ttl','ddmmmyyyy')"></a></td>
                       <td width="113"><div align="center">s.d </div></td>
                       <td width="395"><a href="javascript:NewCssCal('tx_akhir','yyyymmdd')">
                         <input type="Text" name="tx_akhir" id="tx_akhir" maxlength="25" size="20">
@@ -184,8 +186,24 @@ jQuery(function($) {
                       <td>&nbsp;</td>
                       <td>&nbsp;</td>
                       <td>&nbsp;</td>
-                      <td colspan="4"><input name="tx_ajaran" type="hidden" id="tx_ajaran" value="2010-2011"></td>
-                    </tr>
+                      <td colspan="4"><?
+$date = getdate();
+$year = $date['year'];
+$month = $date['mon'];
+
+if ($month=1|2|3|4|5|6){
+$thn_ajaran=($year-1)."-".$year;
+//echo $thn_ajaran;
+?>
+<input name="tx_ajaran" type="hidden" id="tx_ajaran" value="<? echo $thn_ajaran; ?>"></td></tr>
+<? }
+else if($month=7|8|9|10|11|12){
+$thn_ajaran= ($year)."-".($year+1);
+//echo $thn_ajaran;
+?>
+<input name="tx_ajaran" type="hidden" id="tx_ajaran" value="<? echo $thn_ajaran; ?>"></td></tr><?
+}
+?>
                     <tr>
                       <td>&nbsp;</td>
                       <td><label></label></td>
