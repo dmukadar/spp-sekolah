2d11a24560a48668d7467ae07cbb1b633e0622e2
Rio Astamal <me@rioastamal.net>
2012-01-06 14:32:13 +0700

    Menambahkan menu untuk tarif khusus: #MAS02

--- application/controllers/tarif_khusus.php
+++ application/controllers/tarif_khusus.php
@@ -154,40 +154,6 @@ class Tarif_khusus extends Alazka_Controller {
 		}
 	}
 	
-	function ajax_get_siswa(){
-		$this->load->model('Vtarif_khusus_model');
-		$get_data=$this->input->get("text");		
-		
-		$data_mas03=$this->Vtarif_khusus_model->get_siswa($get_data);
-		
-		$data=array();
-		$index=0;
-		foreach($data_mas03->result() as $row){		
-			echo $row->namalengkap."|".$row->id."|".$row->namakelas."|".$row->noinduk."\n";
-		}
-		
-	}
-	
-	function update_data($id=0){
-	$this->load->model('Vtarif_khusus_model');
-		$data['data_mas03']=$this->Vtarif_khusus_model->get_all(array('id'=>$id));
-		 $data['page']='tarif_khusus_view';
-		$this->load->view('site/tarif_khusus_view',$data);		
-	}
-	
-	 
-	public function act_update_data($id=0){
-	$this->load->model('Vtarif_khusus_model');
-		$data=array(			
-			'category'=>$this->input->post('tx_kategori'), 
-			'name'=>$this->input->post('tx_tagihan'), 
-			'fare'=>$this->input->post('tx_jumlah'), 
-			'description'=>$this->input->post('tx_ket'), 		
-		);
-		$status=$this->M_mas01->update($id,$data);
-		redirect("data_tarif");
-	}
-	
 	/**
 	 * Method untuk menambahkan CSS baru pada HEAD
 	 *
